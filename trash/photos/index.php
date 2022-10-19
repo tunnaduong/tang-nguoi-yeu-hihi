8ca8c8e8c67cac14cda99805787e6964280dc388
Tunna Duong <tunnaduong@gmail.com>
2022-10-20 06:52:30 +0700

    removed shits

--- trash/photos/index.php
+++
@@ -1,2 +0,0 @@
-<?php require($_SERVER['DOCUMENT_ROOT'] . "/requires/prevent_direct_access.php"); ?>
-<div>Fuck You</div>
--- trash/requires/credits.php
+++
@@ -1,2 +0,0 @@
-<!-- Trang web được lập trình bởi Dương Tùng Anh - FPT University Hanoi -->
-<!-- Mọi thông tin chi tiết xin liên hệ https://facebook.com/tunna.duong/ hoặc 0376953283 -->
